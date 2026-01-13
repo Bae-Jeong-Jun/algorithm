75f4864f982fec81ca3853efcf10f60fda062882
Bae-Jeong-Jun <jongjun10814@naver.com>
2026-01-13 21:40:11 +0900

    Add files via upload

---
+++ PyCharmMiscProject/codingTest/07_bin_search/bj_1300.py
@@ -0,0 +1,18 @@
+n = int(input())
+k = int(input())
+
+left, right = 1, k
+answer = 0
+while left <= right:
+    mid = (left+right)//2
+    count = 0
+    for i in range(1,n+1):
+        count += min(n, mid//i)
+
+    if count >= k:
+        answer = mid
+        right = mid-1
+    else:
+        left = mid+1
+
+print(answer)
