123f58e826c1e8bb747bb493a0e4e09b8f53484a
Bae-Jeong-Jun <jongjun10814@naver.com>
2026-04-02 20:09:15 +0900

    Add files via upload

---
+++ PyCharmMiscProject/codingTest/programmers/two_circle.py
@@ -0,0 +1,17 @@
+import math
+
+def solution(r1, r2):
+    answer = 0
+    for x in range(1, r2+1):
+        # 큰원
+        max_y = int(math.sqrt(r2**2 - x**2))
+
+        # 작은원
+        if x < r1:
+            min_y = math.ceil(math.sqrt(r1**2 - x**2))
+        else:
+            min_y = 0
+
+        answer += max_y - min_y + 1
+
+    return answer *4
